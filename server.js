fc827e0b708e9c9940b5cd2e773cba39d6ebfb05
Patrick Lin <linpatrick11@gmail.com>
2023-06-22 11:02:32 -0400

    update

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/proof.html');
 });
 
+app.get('/data', (req, res) => {
+  res.sendFile(__dirname + '/proof.json');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
